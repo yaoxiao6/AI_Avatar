d88d3b62814992b0e2d9fea443cf7350adbe109c
Yao Xiao <yao.xiao@symtrain.com>
2025-03-26 21:18:19 -0400

    feat: Add Ollama external IP output for potential Cloud Run migration

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -8,6 +8,7 @@ output "node_backend_url" {
   value = google_cloud_run_service.node_backend.status[0].url
 }
 
+# To move Ollama from GKE to Cloud Run, we need to update more than the main.tf. Please check all 4 files in the terraform directory and update the necessary files. AI!
 output "ollama_external_ip" {
   value = data.kubernetes_service.ollama.status.0.load_balancer.0.ingress.0.ip
 }
